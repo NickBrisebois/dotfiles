ade1ae2c80678f83e9eb226eca564fbf61f8c975
NickBrisebois <nickbrisebois12@gmail.com>
2014-07-09 23:48:32 -0400

    Removed failed attempt at python version of install script

--- install.py
+++
@@ -1,9 +0,0 @@
-import os
-
-ignored_files = ["stylish-firefox.css", "install.py", "README.md"]
-ignored_folders = [".git"]
-current_dir = '/home/nick/dotfiles'
-
-for dirs in os.walk(current_dir, topdown=True):
-    dirs[:] = [d for d in dirs if d not in ignored_folders]
-    print(dirs)

f9efb5add1b488bcc5eaacbea5b111a1964ccb4b
NickBrisebois <nickbrisebois12@gmail.com>
2014-07-17 23:15:31 -0400

    Reverted install.sh to the original. My changes just broke it.

--- install.sh
+++ install.sh
@@ -7,18 +7,25 @@
 ########## Variables
 
 dir=~/dotfiles                    # dotfiles directory
-files=".bashrc .vim .compton.conf .vimrc .Xdefaults .config/bspwm .config/sxhkd"    # list of files/folders to symlink in homedir
+olddir=~/dotfiles_old             # old dotfiles backup directory
+files=".bashrc .compton.conf .xinitrc .vimrc .vim .Xdefaults .config/sxhkd .config/bspwm"    # list of files/folders to symlink in homedir
 
 ##########
+
+# create dotfiles_old in homedir
+echo "Creating $olddir for backup of any existing dotfiles in ~"
+mkdir -p $olddir
+echo "...done"
 
 # change to the dotfiles directory
 echo "Changing to the $dir directory"
 cd $dir
+echo "...done"
 
-# creates symlinks for all the files 
-for file in files; do
-    echo "Deleting old files if they exist"
-    rm -r $dir/$file
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+for file in $files; do
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/$file
 done

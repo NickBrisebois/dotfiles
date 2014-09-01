538f4236105b4c6341e3d5fba173ab1253f545ca
NickBrisebois <nickbrisebois12@gmail.com>
2014-09-01 15:02:47 -0400

    Updated install script

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files=".bashrc .compton.conf .xinitrc .vimrc .vim .Xdefaults .config/sxhkd .config/bspwm .ncmpcpp .mpd"    # list of files/folders to symlink in homedir
+files=".bashrc .compton.conf .xinitrc .vimrc .vim .Xdefaults .config/sxhkd .config/bspwm .ncmpcpp .mpd .bin"    # list of files/folders to symlink in homedir
 
 ##########
 
